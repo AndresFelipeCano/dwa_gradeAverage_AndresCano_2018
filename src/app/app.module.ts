d13b786c600a4df18ec5e2bf5e8910533a218531
Andres Felipe Cano <andresfe98@gmail.com>
2018-11-15 16:44:26 -0500

    Construcción de la aplicación primera fase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { UiModule } from './ui/ui.module';
+
+// Component imports
 import { AppComponent } from './app.component';
+import { PokemainComponent } from './components/pokemain/pokemain.component';
+
+// Routing imports
+import { RouterModule, Routes } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
